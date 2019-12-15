5a9899dc8eba24345fc4c74de83625df482acf26
Kirill Shatskiy <shaackiy@gmail.com>
2019-12-15 17:13:19 +0300

    feat: add .saveBackground method to Canyon

--- canyon/canyon.js
+++ canyon/canyon.js
@@ -4,10 +4,25 @@ function Canyon(width, height, color) {
     this.color = color;
 }
 
+Canyon.prototype.saveBackground = function(x, y, canvas) {
+    if (!this.image) {
+        this.image = createImage(this.width, this.height);
+        this.image.copy(
+            canvas,
+            x - this.width / 2,
+            y,
+            this.width,
+            this.height,
+            0,
+            0,
+            this.width,
+            this.height);
+    }
+}
+
 Canyon.prototype.draw = function(x, y) {
     push();
-    noStroke();
-    fill(this.color);
-    rect(x - this.width / 2, y, this.width, this.height);
+    noSmooth();
+    image(this.image, x - this.width / 2, y);
     pop();
 }
