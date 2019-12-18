cdbdd0f19b679a213c14a5e7e74370049c614288
Kirill Shatskiy <shaackiy@gmail.com>
2019-12-18 13:16:53 +0300

    feat: remove background

--- canyon/canyon.js
+++ canyon/canyon.js
@@ -4,25 +4,8 @@ function Canyon(width, height, color) {
     this.color = color;
 }
 
-Canyon.prototype.saveBackground = function(x, y, canvas) {
-    if (!this.image) {
-        this.image = createImage(this.width, this.height);
-        this.image.copy(
-            canvas,
-            x - this.width / 2,
-            y,
-            this.width,
-            this.height,
-            0,
-            0,
-            this.width,
-            this.height);
-    }
-}
-
 Canyon.prototype.draw = function(x, y) {
-    push();
-    noSmooth();
-    image(this.image, x - this.width / 2, y);
-    pop();
+    noStroke();
+    fill(this.color);
+    rect(x - this.width / 2, y, this.width, this.height);
 }
--- sketch.js
+++ sketch.js
@@ -24,32 +24,30 @@ var isPlummeting;
 var jumpHeight;
 var isFound;
 var BACKGROUND_COLOR = [100,155,255];
-var adventurer = new Adventurer();
-var appleItem = new AppleCollectableItem();
-var smallTree = new SmallTree();
-var bigTree = new BigTree();
-var canvasWidth = 1024;
-var canvasHeight = 640;
-var backBackground = new BackBackground(canvasWidth, canvasHeight);
-var frontBackground = new FrontBackground(canvasWidth, canvasHeight);
-var cloudsFrontBackground = new CloudsFrontBackground(canvasWidth, canvasHeight);
-var cloudsBackBackground = new CloudsBackBackground(canvasWidth, canvasHeight);
+var adventurer;
+var appleItem;
+var smallTree;
+var bigTree;
 var canyon;
-var canyonWidth = 150;
+var canyonWidth;
 var lastDirection;
 var LastDirection = {
 	Left: 1,
 	Right: 2,
 };
 var trees_x;
-var canvas;
+var scrollPos;
+var actualPos;
 
 function setup()
 {
-	canvas = createCanvas(canvasWidth, canvasHeight);
+	createCanvas(1024, 640);
 	floorPos_y = height * 3/4;
 	jumpHeight = floorPos_y - 200;
 	gameChar_x = width/2;
+	canyonWidth = 150;
+	actualGameChar_x = gameChar_x;
+	scrollPos = 0;
 	gameChar_y = floorPos_y;
 	item_x = gameChar_x - 50;
 	item_y = floorPos_y;
@@ -67,38 +65,27 @@ function setup()
 }
 
 function preload() {
+	adventurer = new Adventurer();
+	appleItem = new AppleCollectableItem();
+	smallTree = new SmallTree();
+	bigTree = new BigTree();
 	adventurer.load();
 	appleItem.load();
 	smallTree.load();
 	bigTree.load();
-	backBackground.load();
-	frontBackground.load();
-	cloudsFrontBackground.load();
-	cloudsBackBackground.load();
 }
 
 
 function draw()
 {
+	background(BACKGROUND_COLOR)
 	adventurer.onFrameChange(frameCount);
-	cloudsBackBackground.draw(0, 0);
-	cloudsFrontBackground.draw(0, 0);
-	backBackground.draw(0, 0);
-	frontBackground.draw(0, 0);
-	canyon.saveBackground(canyon_x, canyon_y, canvas);
-
 	noStroke();
 	fill(0,155,0);
 	rect(0, floorPos_y, width, height - floorPos_y); //draw some green ground
-
+	push();
+	translate(scrollPos, 0);
 	setGameCharState();
-
-	/**
-	 * I could write a for loop here
-	 * for (var i = 0; i < trees_x.length; i++) {
-	 *     // draw a tree
-	 * }
-	 */
 	trees_x.forEach(function(x, index) {
 		var treeObject = index % 2 ? smallTree : bigTree
 		treeObject.draw(x, floorPos_y)
@@ -107,24 +94,38 @@ function draw()
 		appleItem.draw(item_x, item_y);
 	}
 	canyon.draw(canyon_x, canyon_y);
+	pop();
 	adventurer.draw(gameChar_x, gameChar_y);
 
-	///////////INTERACTION CODE//////////
+	processInteractions();
+}
+
+function processInteractions() {
 	if (isFallingInCanyon) {
 		isFalling = true;
 		gameChar_y += 10;
 		return;
 	}
-	if (gameChar_y === floorPos_y && dist(gameChar_x, gameChar_y, canyon_x, canyon_y) < (canyonWidth / 2)) {
+	if (gameChar_y === floorPos_y && dist(actualGameChar_x, gameChar_y, canyon_x, canyon_y) < (canyonWidth / 2)) {
 		isFalling = true;
 		isFallingInCanyon = true;
 		return;
 	}
 	if (isRight) {
-		gameChar_x += 5;
+		actualGameChar_x += 5;
+		if(gameChar_x < width * 0.8) {
+			gameChar_x  += 5;
+		} else {
+			scrollPos -= 5; // negative for moving against the background
+		}
 	}
 	if (isLeft) {
-		gameChar_x -= 5;
+		actualGameChar_x -= 5;
+		if(gameChar_x > width * 0.2) {
+			gameChar_x -= 5;
+		} else {
+			scrollPos += 5;
+		}
 	}
 	if (gameChar_y === jumpHeight) {
 		isPlummeting = false;
